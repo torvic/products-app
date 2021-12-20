f77f8e4a5cd301f9b5e825a388ba071c5c37b6b9
torvic <130111@unsaac.edu.pe>
2021-12-20 12:48:13 -0500

    Clean project for starting to code

---
+++ src/pages/AboutPage/index.jsx
@@ -0,0 +1,5 @@
+const AboutPage = () => {
+  return <h2>About Page</h2>
+}
+
+export default AboutPage
---
+++ src/pages/ProductDetailsPage/index.jsx
@@ -0,0 +1,5 @@
+const ProductDetailsPage = () => {
+  return <h2>Product details page</h2>
+}
+
+export default ProductDetailsPage
