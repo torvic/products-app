0eaa19481ca2b24e30aa68dd74580bc6a9f7e5f6
torvic <130111@unsaac.edu.pe>
2021-12-21 16:13:30 -0500

    Call the products api & use it in HomePage

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,3 +1,32 @@
-const HomePage = () => <h2>Home page</h2>;
+import { useEffect, useState } from 'react';
+import ProductList from './components/ProductList';
+import getProducts from '../../api/products/productAPI';
+
+const HomePage = () => {
+  const [dbProducts, setDbProducts] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    setLoading(true);
+    const getAllProducts = async () => {
+      try {
+        const products = await getProducts();
+        setDbProducts(products);
+        setLoading(false);
+      } catch (err) {
+        setLoading(false);
+        setError(true);
+      }
+    };
+    getAllProducts();
+  }, []);
+  return (
+    <>
+      {loading && <div>Cargando...</div>}
+      {error && <div>Ocurrio un error...</div>}
+      {dbProducts && <ProductList items={dbProducts} />}
+    </>
+  );
+};
 
 export default HomePage;
