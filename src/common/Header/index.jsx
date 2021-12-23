30085f1427189e441588cc46234985bcc1808d89
torvic <130111@unsaac.edu.pe>
2021-12-23 10:50:06 -0500

    Add navbar on mobile screen

--- src/common/Header/index.jsx
+++ src/common/Header/index.jsx
@@ -1,29 +1,44 @@
+import { useState } from 'react';
 import { BiMenuAltRight } from 'react-icons/bi';
 import { Link } from 'react-router-dom';
-// import { AiOutlineClose } from 'react-icons/ai';
+import { AiOutlineClose } from 'react-icons/ai';
 import styles from './index.module.scss';
 
-const Header = () => (
-  <header className={styles.header}>
-    <div className={styles.header__content}>
-      <Link to="/" className={styles.header__content_logo}>
-        Torvic
-      </Link>
-      <nav className={styles.header__content_nav}>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-        </ul>
-      </nav>
-      <div className={styles.header__content_toggle}>
-        <BiMenuAltRight />
+const Header = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+  const menuToggleHandler = () => {
+    setMenuOpen((prev) => !prev);
+  };
+  return (
+    <header className={styles.header}>
+      <div className={styles.header__content}>
+        <Link to="/" className={styles.header__content_logo}>
+          Torvic
+        </Link>
+        <nav
+          className={`${styles.header__content_nav} ${
+            menuOpen ? styles.isMenu : ''
+          }`}
+        >
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+          </ul>
+        </nav>
+        <div className={styles.header__content_toggle}>
+          {menuOpen ? (
+            <AiOutlineClose onClick={menuToggleHandler} />
+          ) : (
+            <BiMenuAltRight onClick={menuToggleHandler} />
+          )}
+        </div>
       </div>
-    </div>
-  </header>
-);
+    </header>
+  );
+};
 
 export default Header;
